f9cc9bcab870caa98b77d9ac7311c562276b2e82
venelinatanasov <venelin.atanasov2@gmail.com>
2019-06-23 22:00:08 +0300

    Add files via upload

--- sort.py
+++ sort.py
@@ -2,21 +2,39 @@ import threading
 import time
 import random
 
-local_threads=8
-local_arrs=8
 
 
-def msort(arr):
-    arr.sort()
+local_arrs=10
+local_threads=local_arrs#za vseki slu4ai
+arr_size=100000
 
 
-def run_local(local_threads,arr):
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter,arr):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+        self.arr=arr
+    def run(self):
+        print ("Starting " + self.name)
+        self.arr.sort()
+        print ("Exiting " + self.name)
+
+
+
+
+
+
+
+
+def run_local(local_arrs,arr):#podava se celiq dvumeren!!!
     threads=[]
-    for i in range(local_threads):
-        thread = threading.Thread(target=msort, args=(arr[i],))
+    for i in range(local_arrs):
+        thread = myThread(i,"Thread-"+str(i),i,arr[i])
         threads.append(thread)
         thread.start()
-    for i in range(local_threads):
+    for i in range(local_arrs):
         threads[i].join()
 
 
@@ -24,19 +42,29 @@ def run_local(local_threads,arr):
 arr=[]
 arrs=[]
 for i in range(local_arrs):
-    for i in range(5000):
+    for k in range(arr_size):
         arr.append(random.randint(0,1000))
     arrs.append(arr)
+    arr=[]
 
 
+#print(arrs)
+arrs2=arrs
 
+start=time.time()
+run_local(local_arrs,arrs)#podavam dvumerniq!!
+end=time.time()
+
+print("Multi: "+str(end-start))
+
+start=time.time()
 for i in range(local_arrs):
-    arrs.append(arr)
-    for k in range(5000):
-        arrs[i].append(random.randint)
+    arrs2[i].sort()
+end=time.time()
 
-run_local(local_threads,arrs)
+print("Single: "+str(end-start))
 
+#print(arrs)
 
 
 
