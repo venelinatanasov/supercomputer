5f02c0043becd8413fd3b6937bcc61627be21666
venelinatanasov <venelin.atanasov2@gmail.com>
2019-06-24 22:14:44 +0300

    Add files via upload

--- sort.py
+++ sort.py
@@ -1,12 +1,12 @@
 import threading
 import time
 import random
-
+from multiprocessing import Process
 
 
 local_arrs=10
 local_threads=local_arrs#za vseki slu4ai
-arr_size=100000
+arr_size=10
 
 
 class myThread (threading.Thread):
@@ -19,25 +19,38 @@ class myThread (threading.Thread):
     def run(self):
         print ("Starting " + self.name)
         self.arr.sort()
+        print(arr)
         print ("Exiting " + self.name)
 
 
+def msort(arr):
+    arr.sort()
+    print(arr)
 
 
 
 
-
+#def run_local(local_arrs,arr):#podava se celiq dvumeren!!!
+#    threads=[]
+#    for i in range(local_arrs):
+#        thread = myThread(i,"Thread-"+str(i),i,arr[i])
+#        threads.append(thread)
+#        thread.start()
+#    for i in range(local_arrs):
+#        threads[i].join()
 
 def run_local(local_arrs,arr):#podava se celiq dvumeren!!!
-    threads=[]
+    processes=[]
     for i in range(local_arrs):
-        thread = myThread(i,"Thread-"+str(i),i,arr[i])
-        threads.append(thread)
-        thread.start()
-    for i in range(local_arrs):
-        threads[i].join()
+        process = Process(target=msort, args=(arr[i],))
+        processes.append(process)
+
+    for process in processes:
+        process.start()
 
 
+    for process in processes:
+        process.join()
 
 arr=[]
 arrs=[]
@@ -54,16 +67,17 @@ arrs2=arrs
 start=time.time()
 run_local(local_arrs,arrs)#podavam dvumerniq!!
 end=time.time()
+print(arrs)
+print("Multi proc: "+str(end-start))
+#print(arrs2)
+#start=time.time()
+#for i in range(local_arrs):
+ #   arrs2[i].sort()
+#end=time.time()
 
-print("Multi: "+str(end-start))
-
-start=time.time()
-for i in range(local_arrs):
-    arrs2[i].sort()
-end=time.time()
-
-print("Single: "+str(end-start))
-
+#print("Single: "+str(end-start))
+#print(arrs)
+#print(arrs2)
 #print(arrs)
 
 
