277289d21e9d897e0f009046b75beac491f95f27
Boyan1312 <51244720+Boyan1312@users.noreply.github.com>
2019-06-25 19:15:05 +0300

    Add files via upload

--- razpredelqne.py
+++ razpredelqne.py
@@ -4,12 +4,21 @@ core_count = 19
 
 # Computer
 class Computer(object):
-    def __init__(self, ip, file, arrays):
+    def __init__(self, ip, file, corecount, arrays):
         self.ip = ip
         self.file = file
+        self.corecount = corecount
         self.arrays = arrays
 
-Computer1 = Computer('192.168.91.2', 'masi.txt', 0)
+#Computerlist
+Computer1 = Computer('192.168.91.1', 'masi.txt', 8, 0)
+Computer2 = Computer('192.168.91.2', 'masi.txt', 4, 0)
+Computer3 = Computer('192.168.91.3', 'masi.txt', 2, 0)
+Computer4 = Computer('192.168.91.4', 'masi.txt', 3, 0)
+Computer5 = Computer('192.168.91.5', 'masi.txt', 1, 0)
+Computer6 = Computer('192.168.91.6', 'masi.txt', 1, 0)
+
+computers = [Computer1, Computer2, Computer3, Computer4, Computer5, Computer6]
 
 #Deviding algorithm
 with open('masiv', 'rb') as FILE:
@@ -20,10 +29,16 @@ v = len(masivlist) % core_count
 t = int((len(masivlist)) / core_count)
 
 
-Computer1.arrays += t
-Computer1.arrays += v
+for i in computers:
+    i.arrays += t * i.corecount
+
+for j in computers:
+    j.arrays += 1
+    v -= 1
+    if v == 0:
+        break;
 
 print(len(masivlist))
-print(v)
-print(Computer1.arrays)
-print(t)
+
+for u in computers:
+    print(u.arrays)
